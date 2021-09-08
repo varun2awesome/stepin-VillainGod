3139c43307b50f7223c853ef00db5a42c87b9681
Varun Verma <64241983+varun2awesome@users.noreply.github.com>
2021-09-08 14:37:32 +0530

    Update secret_keeper.c

--- 3_Implementation/src/secret_keeper.c
+++ 3_Implementation/src/secret_keeper.c
@@ -1,1100 +1,563 @@
 #include "villain.h"
-
 // This function adds the record at a particular day and time in secret keeper
 void addrecord( )
-
 {
-
     system("cls");
-
     FILE *fp ;
-
     char another = 'Y' ,time[10];
-
     struct record e ;
-
     char filename[15];
-
     int choice;
-
     printf("\n\n\t\t✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠\n");
-
     printf("\t\t* WELCOME TO THE ADD MENU *");
-
     printf("\n\t\t✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠\n\n");
-
     printf("\n\n\tENTER DATE OF YOUR SECRET:[yyyy-mm-dd]:");
-
     fflush(stdin);
-
     fgets(filename,15,stdin);
-
     fp = fopen (filename, "ab+" ) ;
-
     if ( fp == NULL )
-
     {
-
         fp=fopen(filename,"wb+");
-
         if(fp==NULL)
-
         {
-
             printf("\nSYSTEM ERROR...");
-
             printf("\nPRESS ANY KEY TO EXIT");
-
             getchar();
-
             return ;
-
         }
-
     }
     // if the user wants to add secret on the same day but different time then this is done here
     while ( another == 'Y'|| another=='y' )
-
     {
-
         choice=0;
-
         fflush(stdin);
-
         printf ( "\n\tENTER TIME:[hh:mm]:");
-
         scanf("%s",time);
-
         rewind(fp);
-
         while(fread(&e,sizeof(e),1,fp)==1)
-
         {
-
             if(strcmp(e.time,time)==0)
-
             {
-
                 printf("\n\tALREADY EXISTS.\n");
-
                 choice=1;
-
             }
-
         }
-
         if(choice==0)
-
         {
-
             strcpy(e.time,time);
-
             printf("\tENTER NAME:");
-
             fflush(stdin);
-
             fgets(e.name,30,stdin);
-
             fflush(stdin);
-
             printf("\tENTER PLACE:");
-
             fgets(e.place,25,stdin);
-
             fflush(stdin);
-
             printf("\tENTER DURATION:");
-
             fgets(e.duration,10,stdin);
-
             fflush(stdin);
-
             printf("\tNOTE:");
-
             fgets(e.note,500,stdin);
-
             fwrite ( &e, sizeof ( e ), 1, fp ) ;
-
             printf("\nsssss! SECRET ADDED...\n");
-
         }
-
         printf ( "\n\tWANNA ADD ANOTHER SECRET...(Y/N) " ) ;
-
         fflush ( stdin ) ;
-
         another = getchar( ) ;
-
     }
-
     fclose ( fp ) ;
-
     printf("\n\n\tPRESS ANY KEY TO EXIT...");
-
     getchar();
-
 }
-
 // This function is to view record already entered by the user
 void viewrecord( )
-
 {
-
     FILE *fpte ;
-
     system("cls");
-
     struct record customer ;
-
     char time[6],choice,filename[14];
-
     int ch;
-
     printf("\n\n\t\t✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠\n");
-
     printf("\t\t* VIEWING SECRETS *");
-
     printf("\n\t\t✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠\n\n");
-
     choice=password();
-
     if(choice!=0)
-
     {
-
         return ;
-
     }
-
     do
-
     {
     // To view the record of a particular day
         printf("\n\tENTER THE DATE OF SECTRET TO BE VIEWED:[yyyy-mm-dd]:");
-
         fflush(stdin);
-
         fgets(filename,14,stdin);
-
         fpte = fopen ( filename, "rb" ) ;
-
         if ( fpte == NULL )
-
         {
-
             puts ( "\n DO NOT EXIST...\n" ) ;
-
             printf("PRESS ANY KEY TO EXIT...");
-
             getchar();
-
             return ;
-
         }
-
         system("cls");
-
         // Menu-driven system to view the record based on frequent conditions
-
         printf("\n\tHOW WOULD YOU LIKE TO VIEW:\n");
-
         printf("\n\t1.WHOLE SECRETS OF THE DAY.");
-
         printf("\n\t2.SECRETS OF FIX TIME.");
-
         printf("\n\t\tENTER YOUR CHOICE:");
-
         scanf("%d",&ch);
-
         switch(ch)
-
         {
-
         case 1:
-
             printf("\nALL THE SECRETS FOR %s IS:",filename);
-
             while ( fread ( &customer, sizeof ( customer ), 1, fpte ) == 1 )
-
             {
-
                 printf("\n");
-
                 printf("\nTIME: %s",customer.time);
-
                 printf("\nMEETING WITH: %s",customer.name);
-
                 printf("\nMEETING AT: %s",customer.place);
-
                 printf("\nDURATION: %s",customer.duration);
-
                 printf("\nNOTE: %s",customer.note);
-
                 printf("\n");
-
             }
-
             break;
-
         case 2:
-
             fflush(stdin);
-
             printf("\nENTER TIME:[hh:mm]:");
-
             fgets(time,6,stdin);
-
             while ( fread ( &customer, sizeof ( customer ), 1, fpte ) == 1 )
-
             {
-
                 if(strcmp(customer.time,time)==0)
-
                 {
-
                     printf("\nYOUR SECRET IS:");
-
                     printf("\nTIME: %s",customer.time);
-
                     printf("\nMEETING WITH: %s",customer.name);
-
                     printf("\nMEETING AT: %s",customer.place);
-
                     printf("\nDUARATION: %s",customer.duration);
-
                     printf("\nNOTE: %s",customer.note);
-
                 }
-
             }
-
             break;
-
         default:
             printf("\nYOU TYPED SOMETHING ELSE...\n");
-
             break;
-
         }
-
         printf("\n\nWOULD YOU LIKE TO CONTINUE VIEWING...(Y/N):");
-
         fflush(stdin);
-
         scanf("%c",&choice);
-
     }
     while(choice=='Y'||choice=='y');
-
     fclose ( fpte ) ;
-
     return ;
-
 }
-
 //This function helps to edit the record
-
 void editrecord()
-
 {
-
     system("cls");
-
     FILE *fpte ;
-
     struct record customer ;
-
     char time[6],choice,filename[14];
-
     int num,count=0;
-
     printf("\n\n\t\t✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠\n");
-
     printf("\t\t* WELCOME TO THE EDITING MENU *");
-
     printf("\n\t\t✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠\n\n");
-
     choice=password();
-
     if(choice!=0)
-
     {
-
         return ;
-
     }
-
     do
-
     {
-
         printf("\n\tENTER THE DATE OF RECORD TO BE EDITED:[yyyy-mm-dd]:");
-
         fflush(stdin);
-
         fgets(filename,14,stdin);
-
         printf("\n\tENTER TIME:[hh:mm]:");
-
         fgets(time,6,stdin);
-
         fpte = fopen ( filename, "rb+" ) ;
-
         if ( fpte == NULL )
-
         {
-
             printf( "\nRECORD DOES NOT EXISTS:" ) ;
-
             printf("\nPRESS ANY KEY TO GO BACK");
-
             getchar();
-
             return;
-
         }
-
         while ( fread ( &customer, sizeof ( customer ), 1, fpte ) == 1 )
-
         {
-
             if(strcmp(customer.time,time)==0)
-
             {
-
                 printf("\nYOUR OLD RECORD WAS AS:");
-
                 printf("\nTIME: %s",customer.time);
-
                 printf("\nMEETING WITH: %s",customer.name);
-
                 printf("\nMEETING AT: %s",customer.place);
-
                 printf("\nDURATION: %s",customer.duration);
-
                 printf("\nNOTE: %s",customer.note);
-
                 printf("\n\n\t\tWHAT WOULD YOU LIKE TO EDIT..");
-
                 printf("\n1.TIME.");
-
                 printf("\n2.MEETING PERSON.");
-
                 printf("\n3.MEETING PLACE.");
-
                 printf("\n4.DURATION.");
-
                 printf("\n5.NOTE.");
-
                 printf("\n6.WHOLE RECORD.");
-
                 printf("\n7.GO BACK TO MAIN MENU.");
-
                 do
-
                 {
-
                     printf("\n\tENTER YOUR CHOICE:");
-
                     fflush(stdin);
-
                     scanf("%d",&num);
-
                     fflush(stdin);
-
                     switch(num)
-
                     {
-
                     case 1:
                         printf("\nENTER THE NEW DATA:");
-
                         printf("\nNEW TIME:[hh:mm]:");
-
                         fgets(customer.time,6,stdin);
-
                         break;
-
                     case 2:
                         printf("\nENTER THE NEW DATA:");
-
                         printf("\nNEW MEETING PERSON:");
-
                         fgets(customer.name,30,stdin);
-
                         break;
-
                     case 3:
                         printf("\nENTER THE NEW DATA:");
-
                         printf("\nNEW MEETING PLACE:");
-
                         fgets(customer.place,25,stdin);
-
                         break;
-
                     case 4:
                         printf("\nENTER THE NEW DATA:");
-
                         printf("\nDURATION:");
-
                         fgets(customer.duration,10,stdin);
-
                         break;
-
                     case 5:
                         printf("ENTER THE NEW DATA:");
-
                         printf("\nNOTE:");
-
                         fgets(customer.note,500,stdin);
-
                         break;
-
                     case 6:
                         printf("\nENTER THE NEW DATA:");
-
                         printf("\nNEW TIME:[hh:mm]:");
-
                         fgets(customer.time,6,stdin);
-
                         printf("\nNEW MEETING PERSON:");
-
                         fgets(customer.name,30,stdin);
-
                         printf("\nNEW MEETING PLACE:");
-
                         fgets(customer.place,25,stdin);
-
                         printf("\nDURATION:");
-
                         fgets(customer.duration,10,stdin);
-
                         printf("\nNOTE:");
-
                         fgets(customer.note,500,stdin);
-
                         break;
-
                     case 7:
                         printf("\nPRESS ANY KEY TO GO BACK...\n");
-
                         getchar();
-
                         return ;
-
                         break;
-
                     default:
                         printf("\nYOU TYPED SOMETHING ELSE...TRY AGAIN\n");
-
                         break;
-
                     }
-
                 }
                 while(num<1||num>8);
-
                 fseek(fpte,-sizeof(customer),SEEK_CUR);
-
                 fwrite(&customer,sizeof(customer),1,fpte);
-
                 fseek(fpte,-sizeof(customer),SEEK_CUR);
-
                 fread(&customer,sizeof(customer),1,fpte);
-
                 choice=5;
-
                 break;
-
             }
-
         }
-
         if(choice==5)
-
         {
-
             system("cls");
-
             printf("\n\t\tEDITING COMPLETED...\n");
-
             printf("--------------------\n");
-
             printf("THE NEW RECORD IS:\n");
-
             printf("--------------------\n");
-
             printf("\nTIME: %s",customer.time);
-
             printf("\nMEETING WITH: %s",customer.name);
-
             printf("\nMEETING AT: %s",customer.place);
-
             printf("\nDURATION: %s",customer.duration);
-
             printf("\nNOTE: %s",customer.note);
-
             fclose(fpte);
-
             printf("\n\n\tWOULD YOU LIKE TO EDIT ANOTHER RECORD.(Y/N)");
-
             scanf("%c",&choice);
-
             count++;
-
         }
-
         else
-
         {
-
             printf("\nTHE RECORD DOES NOT EXIST::\n");
-
             printf("\nWOULD YOU LIKE TO TRY AGAIN...(Y/N)");
-
             scanf("%c",&choice);
-
         }
-
     }
     while(choice=='Y'||choice=='y');
-
     fclose ( fpte ) ;
-
     if(count==1)
-
         printf("\n%d FILE IS EDITED...\n",count);
-
     else if(count>1)
-
         printf("\n%d FILES ARE EDITED..\n",count);
-
     else
-
         printf("\nNO FILES EDITED...\n");
-
     printf("\tPRESS ENTER TO EXIT EDITING MENU.");
-
     getchar();
-
 }
 // Password function is called to authenticate the user with the maximum of three trials only
 int password()
-
 {
-
     char pass[15]= {0},check[15]= {0},ch;
-
     FILE *fpp;
-
     int i=0,j;
-
     printf("::FOR SECURITY PURPOSE::");
-
     printf("::ONLY THREE TRIALS ARE ALLOWED::");
-
     for(j=0; j<3; j++)
-
     {
-
         i=0;
-
         printf("\n\n\tENTER THE PASSWORD:");
-
         pass[0]=getchar();
-
         while(pass[i]!='\r')
-
         {
-
             if(pass[i]=='\b')
-
             {
-
                 i--;
-
                 printf("\b");
-
                 printf(" ");
-
                 printf("\b");
-
                 pass[i]=getchar();
-
             }
-
             else
-
             {
-
                 printf("*");
-
                 i++;
-
                 pass[i]=getchar();
-
             }
-
         }
-
         pass[i]='\0';
-
         fpp=fopen("SE","r");
-
         if (fpp==NULL)
-
         {
-
             printf("\nERROR WITH THE SYSTEM FILE...[FILE MISSING]\n");
-
             getchar();
-
             return 1;
-
         }
-
         else
-
             i=0;
-
         while(1)
-
         {
-
             ch=fgetc(fpp);
-
             if(ch==EOF)
-
             {
-
                 check[i]='\0';
-
                 break;
-
             }
-
             check[i]=ch-5;
-
             i++;
-
         }
         // Comparison of password is done with the help of strcmp function
         if(strcmp(pass,check)==0)
-
         {
-
             printf("\n\n\tACCESS GRANTED...\n");
-
             return 0;
-
         }
-
         else
-
         {
-
             printf("\n\n\tWRONG PASSWORD..\n\n\tACCESS DENIED...\n");
-
         }
-
     }
-
     printf("\n\n\t::YOU ENTERED WRONG PASSWORD::YOU ARE NOT ALLOWED TO ACCESS ANY FILE::\n\n\tPRESS ANY KEY TO GO BACK...");
-
     getchar();
-
     return 1;
-
 }
-
 // This function helps the user to edit password.
-
 void editpassword()
-
 {
-
     system("cls");
-
     printf("\n");
-
     char pass[15]= {0},confirm[15]= {0},ch;
-
     int choice,i,check;
-
     FILE *fp;
-
     fp=fopen("SE","rb");
-
     if(fp==NULL)
-
     {
-
         fp=fopen("SE","wb");
-
         if(fp==NULL)
-
         {
-
             printf("SYSTEM ERROR...");
-
             getchar();
-
             return ;
-
         }
-
         fclose(fp);
-
         printf("\nSYSTEM RESTORED...\nYOUR PASSWORD IS 'ENTER'\n PRESS ENTER TO CHANGE PASSWORD\n\n");
-
         getchar();
-
     }
-
     fclose(fp);
-
     check=password();
-
     if(check==1)
-
     {
-
         return ;
-
     }
-
     do
-
     {
-
         if(check==0)
-
         {
-
             i=0;
-
             choice=0;
-
             printf("\n\n\tENTER THE NEW PASSWORD:");
-
             fflush(stdin);
-
             pass[0]=getchar();
-
             while(pass[i]!='\r')
-
             {
-
                 if(pass[i]=='\b')
-
                 {
-
                     i--;
-
                     printf("\b");
-
                     printf(" ");
-
                     printf("\b");
-
                     pass[i]=getchar();
-
                 }
-
                 else
-
                 {
-
                     printf("*");
-
                     i++;
-
                     pass[i]=getchar();
-
                 }
-
             }
-
             pass[i]='\0';
-
             i=0;
-
             printf("\n\tCONFIRM PASSWORD:");
-
             confirm[0]=getchar();
-
             while(confirm[i]!='\r')
-
             {
-
                 if(confirm[i]=='\b')
-
                 {
-
                     i--;
-
                     printf("\b");
-
                     printf(" ");
-
                     printf("\b");
-
                     confirm[i]=getchar();
-
                 }
-
                 else
-
                 {
-
                     printf("*");
-
                     i++;
-
                     confirm[i]=getchar();
-
                 }
-
             }
-
             confirm[i]='\0';
-
             if(strcmp(pass,confirm)==0)
-
             {
-
                 fp=fopen("SE","wb");
-
                 if(fp==NULL)
-
                 {
-
                     printf("\n\t\tSYSTEM ERROR");
-
                     getchar();
-
                     return ;
-
                 }
-
                 i=0;
-
                 while(pass[i]!='\0')
-
                 {
-
                     ch=pass[i];
-
                     putc(ch+5,fp);
-
                     i++;
-
                 }
-
                 putc(EOF,fp);
-
                 fclose(fp);
-
             }
-
             else
-
             {
-
                 printf("\n\tTHE NEW PASSWORD DOES NOT MATCH.");
-
                 choice=1;
-
             }
-
         }
-
     }
     while(choice==1);
-
     printf("\n\n\tPASSWORD CHANGED...\n\n\tPRESS ANY KEY TO GO BACK...");
-
     getchar();
-
 }
-
 // This function helps the user to delete the records
-
 void deleterecord( )
-
 {
-
     system("cls");
-
     FILE *fp,*fptr ;
-
     struct record file ;
-
     char filename[15],another = 'Y' ,time[10];;
-
     int choice,check;
-
     printf("\n\n\t\✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠\n");
-
     printf("\t\t* WELCOME TO DELETE MENU*");
-
     printf("\n\t\t✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠✠\n\n");
-
     check = password();
-
     if(check==1)
-
     {
-
         return ;
-
     }
-
     while ( another == 'Y' )
-
     // This is menu driven system for user to select the way to delete the record.
     {
-
         printf("\n\n\tHOW WOULD YOU LIKE TO DELETE.");
-
         printf("\n\n\t#DELETE WHOLE RECORD\t\t\t[1]");
-
         printf("\n\t#DELETE A PARTICULAR RECORD BY TIME\t[2]");
-
         do
-
         {
-
             printf("\n\t\tENTER YOU CHOICE:");
-
             scanf("%d",&choice);
-
             switch(choice)
-
             {
-
             case 1:
-
                 printf("\n\tENTER THE DATE OF RECORD TO BE DELETED:[yyyy-mm-dd]:");
-
                 fflush(stdin);
-
                 fgets(filename,15,stdin);
-
                 fp = fopen (filename, "wb" ) ;
-
                 if ( fp == NULL )
-
                 {
-
                     printf("\nTHE FILE DOES NOT EXISTS");
-
                     printf("\nPRESS ANY KEY TO GO BACK.");
-
                     getchar();
-
                     return ;
-
                 }
-
                 fclose(fp);
-
                 remove(filename);
-
                 printf("\nDELETED SUCCESFULLY...");
-
                 break;
-
             case 2:
-
                 printf("\n\tENTER THE DATE OF RECORD:[yyyy-mm-dd]:");
-
                 fflush(stdin);
-
                 fgets(filename,15,stdin);
-
                 fp = fopen (filename, "rb" ) ;
-
                 if ( fp == NULL )
-
                 {
-
                     printf("\nTHE FILE DOES NOT EXISTS");
-
                     printf("\nPRESS ANY KEY TO GO BACK.");
-
                     getchar();
-
                     return ;
-
                 }
-
                 fptr=fopen("temp","wb");
-
                 if(fptr==NULL)
-
                 {
-
                     printf("\nSYSTEM ERROR");
-
                     printf("\nPRESS ANY KEY TO GO BACK");
-
                     getchar();
-
                     return ;
-
                 }
-
                 printf("\n\tENTER THE TIME OF RECORD TO BE DELETED:[hh:mm]:");
-
                 fflush(stdin);
-
                 fgets(time,6,stdin);
-
                 while(fread(&file,sizeof(file),1,fp)==1)
-
                 {
-
                     if(strcmp(file.time,time)!=0)
-
                         fwrite(&file,sizeof(file),1,fptr);
-
                 }
-
                 fclose(fp);
-
                 fclose(fptr);
-
                 remove(filename);
-
                 rename("temp",filename);
-
                 printf("\nDELETED SUCCESFULLY...");
-
                 break;
-
             default:
-
                 printf("\n\tYOU ENTERED WRONG CHOICE");
-
                 break;
-
             }
-
         }
         while(choice<1||choice>2);
-
         printf("\n\tDO YOU LIKE TO DELETE ANOTHER RECORD.(Y/N):");
-
         fflush(stdin);
-
         scanf("%c",&another);
-
     }
-
     printf("\n\n\tPRESS ANY KEY TO EXIT...");
-
     getchar();
-
 }
